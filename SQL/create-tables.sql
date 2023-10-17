df797107a6e06f2e5b6f0666eaed63dd0292edd9
Villalobos4113 <villalobos@fvb.one>
2023-10-17 11:11:47 -0600

    Added DB related info

---
+++ SQL/create-tables.sql
@@ -0,0 +1,105 @@
+CREATE TABLE Gender (
+  id INT PRIMARY KEY,
+  gender VARCHAR(255)
+);
+
+CREATE TABLE PartsOfDay (
+  id INT PRIMARY KEY,
+  part_of_day VARCHAR(255)
+);
+
+CREATE TABLE HoursOfDay (
+  id INT PRIMARY KEY,
+  hour_of_day VARCHAR(255)
+);
+
+CREATE TABLE Exercises (
+  id INT PRIMARY KEY,
+  exercise VARCHAR(255)
+);
+
+CREATE TABLE Intensity (
+  id INT PRIMARY KEY,
+  intensity VARCHAR(255)
+);
+
+CREATE TABLE Medicine (
+  id INT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE [User] (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  date_birth DATE,
+  gender_id INT FOREIGN KEY REFERENCES Gender(id)
+);
+
+CREATE TABLE Doctor (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  address VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(255)
+);
+
+CREATE TABLE Report (
+  id INT PRIMARY KEY,
+  created_at DATE,
+  glucose DECIMAL,
+  ketones DECIMAL,
+  notes TEXT,
+  user_id INT FOREIGN KEY REFERENCES [User](id),
+  part_of_day_id INT FOREIGN KEY REFERENCES PartsOfDay(id)
+);
+
+CREATE TABLE ReportMedicines (
+  id INT PRIMARY KEY,
+  quantity INT,
+  report_id INT FOREIGN KEY REFERENCES Report(id),
+  medicine_id INT FOREIGN KEY REFERENCES Medicine(id)
+);
+
+CREATE TABLE Tracking (
+  id INT PRIMARY KEY,
+  created_at DATE,
+  start_hour_id INT FOREIGN KEY REFERENCES HoursOfDay(id),
+  end_hour_id INT FOREIGN KEY REFERENCES HoursOfDay(id),
+  exercise_id INT FOREIGN KEY REFERENCES Exercises(id),
+  intensity_id INT FOREIGN KEY REFERENCES Intensity(id),
+  calories INT,
+  notes TEXT,
+  user_id INT FOREIGN KEY REFERENCES [User](id)
+);
+
+CREATE TABLE FollowUp (
+  id INT PRIMARY KEY,
+  date DATE,
+  notes TEXT,
+  user_id INT FOREIGN KEY REFERENCES [User](id),
+  doctor_id INT FOREIGN KEY REFERENCES Doctor(id)
+);
+
+CREATE TABLE UserMedicine (
+  id INT PRIMARY KEY,
+  quantity INT,
+  user_id INT FOREIGN KEY REFERENCES [User](id),
+  medicine_id INT FOREIGN KEY REFERENCES Medicine(id)
+);
+
+CREATE TABLE ForumMessage (
+  id INT PRIMARY KEY,
+  message TEXT,
+  created_at DATETIME,
+  user_id INT FOREIGN KEY REFERENCES [User](id)
+);
+
+CREATE TABLE UserDoctor (
+  user_id INT FOREIGN KEY REFERENCES [User](id),
+  doctor_id INT FOREIGN KEY REFERENCES Doctor(id),
+  PRIMARY KEY (user_id, doctor_id)
+);
---
+++ SQL/fill-catalogs.sql
@@ -0,0 +1,76 @@
+-- Insert into Gender
+INSERT INTO Gender (id, gender) VALUES
+(1, 'Masculino'),
+(2, 'Femenino'),
+(3, 'Otro');
+
+-- Insert into PartsOfDay
+INSERT INTO PartsOfDay (id, part_of_day) VALUES
+(1, 'Antes de Desayunar'),
+(2, 'Después de Desayunar'),
+(3, 'Antes de Comer');
+(4, 'Después de Comer');
+(5, 'Antes de Cenar');
+(6, 'Después de Cenar');
+(7, 'Antes de Dormir');
+
+-- Insert into HoursOfDay
+INSERT INTO HoursOfDay (id, hour_of_day) VALUES
+(1, '01:00'),
+(2, '02:00'),
+(3, '03:00'),
+(4, '04:00'),
+(5, '05:00'),
+(6, '06:00'),
+(7, '07:00'),
+(8, '08:00'),
+(9, '09:00'),
+(10, '10:00'),
+(11, '11:00'),
+(12, '12:00'),
+(13, '13:00'),
+(14, '14:00'),
+(15, '15:00'),
+(16, '16:00'),
+(17, '17:00'),
+(18, '18:00'),
+(19, '19:00'),
+(20, '20:00'),
+(21, '21:00'),
+(22, '22:00'),
+(23, '23:00'),
+(24, '00:00');
+
+-- Insert into Exercises
+INSERT INTO Exercises (id, exercise) VALUES
+(1, 'Correr'),
+(2, 'Nadar'),
+(3, 'Ciclismo'),
+(4, 'Levantamiento de pesas'),
+(5, 'Yoga');
+(6, 'Pilates'),
+(7, 'Aeróbicos'),
+(8, 'Zumba'),
+(9, 'Baile de salón'),
+(10, 'Senderismo'),
+(11, 'Escalada'),
+(12, 'Boxeo'),
+(13, 'Artes marciales'),
+(14, 'Entrenamiento funcional'),
+(15, 'Crossfit'),
+(16, 'Spinning'),
+(17, 'Natación sincronizada'),
+(18, 'Baloncesto'),
+(19, 'Fútbol'),
+(20, 'Vóley playa'),
+(21, 'Tenis'),
+(22, 'Gimnasia rítmica'),
+(23, 'Escalada en roca'),
+(24, 'Surf'),
+(25, 'Esquí alpino');
+
+-- Insert into Intensity
+INSERT INTO Intensity (id, intensity) VALUES
+(1, 'Baja'),
+(2, 'Media'),
+(3, 'Alta');
