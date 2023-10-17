208a7f4634a42dfed0ee09d2417ae6ffe377bc07
Villalobos4113 <villalobos@fvb.one>
2023-10-16 20:13:26 -0600

    Created DB Connection

---
+++ ProyectoDAI/ConnectionDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDAI
+{
+    public class ConnectionDB
+    {
+        public OdbcConnection con { get; set; }
+
+        public ConnectionDB()
+        {
+            System.Configuration.Configuration webConfig;
+
+            webConfig = System.Web.Configuration
+                .WebConfigurationManager
+                .OpenWebConfiguration("/dia-well");
+
+            System.Configuration.ConnectionStringSettings connectionString;
+
+            connectionString = webConfig.ConnectionStrings
+                .ConnectionStrings["DefaultConnection"];
+
+            con = new OdbcConnection(connectionString.ToString());
+
+            con.Open();
+        }
+    }
+}
